051bc604eb30208367a7be5ae997d30d0c26083c
Brett Jones <brettjonesdev@gmail.com>
2019-07-31 09:42:17 -1000

    Use NVM instead of ASDF for Node version mgmt

--- bin/paths.sh
+++ bin/paths.sh
@@ -3,12 +3,13 @@ export PATH="$HOME/bin:$PATH";
 
 ## Node Stuff ##
 
-# export NVM_DIR="$HOME/.nvm"
-# source $(brew --prefix nvm)/nvm.sh
-# . "/usr/local/opt/nvm/nvm.sh"
-. $HOME/.asdf/asdf.sh
-. $HOME/.asdf/completions/asdf.bash
+export NVM_DIR="$HOME/.nvm"
+source $(brew --prefix nvm)/nvm.sh
+. "/usr/local/opt/nvm/nvm.sh"
 
+# ASDF shit for node version management
+# . $HOME/.asdf/asdf.sh
+# . $HOME/.asdf/completions/asdf.bash
 
 ## Ruby ##
 export GEM_HOME=$HOME/gems
