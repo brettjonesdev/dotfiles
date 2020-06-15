d18ca08f5a0b1e93d261d73e9f996ae5632f5008
Brett Jones <brettjonesdev@gmail.com>
2020-06-15 00:18:45 -1000

    Remove duplicate paths.sh call

--- bin/index.sh
+++ bin/index.sh
@@ -6,5 +6,4 @@ source ~/.dotfiles/bin/bash_prompt.sh
 source ~/.dotfiles/bin/env_vars.sh
 source ~/.dotfiles/bin/extra.sh
 source ~/.dotfiles/bin/exports.sh
-source ~/.dotfiles/bin/paths.sh
 source ~/.dotfiles/bin/docker.sh
